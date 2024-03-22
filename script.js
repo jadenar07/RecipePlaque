77f857dd0804788819586cafcffd367f9fe855e0
Jaden Ritchie <147543860+jadenar07@users.noreply.github.com>
2024-03-21 23:21:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Define the API endpoint and your API key
     const apiUrl = "https://api.spoonacular.com/recipes/findByNutrients";
     const apiKey = "9d82b920f7b9463fb8b0858317e310d1"; // Your actual API key
 
-    // Define the parameters including the API key
     const queryParams = {
         apiKey: apiKey,
         "minProtein": 10,
         "maxProtein": 500
     };
 
-    // Convert parameters to query string
     const queryString = Object.keys(queryParams)
         .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(queryParams[key])}`)
         .join('&');
 
-    // API endpoint with query string
     const apiUrlWithQuery = `${apiUrl}?${queryString}`;
 
-    // Function to fetch data from API and display it in HTML
     function fetchDataAndDisplay() {
         fetch(apiUrlWithQuery)
             .then(response => {
@@ -36,19 +31,15 @@ document.addEventListener("DOMContentLoaded", function() {
             });
     }
 
-    // Function to display data in HTML
     function displayDataInHTML(data) {
         const productsContainer = document.getElementById('products-container');
         
-        // Clear previous content
         productsContainer.innerHTML = '';
 
-        // Loop through the products and create HTML elements
         data.forEach(recipe => {
             const recipeElement = document.createElement('div');
             recipeElement.classList.add('recipe');
     
-            // HTML structure for each recipe, including the heart icon
             recipeElement.innerHTML = `
                 <div class="recipe-info">
                     <h2>${recipe.title}</h2>
@@ -69,6 +60,5 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
-    // Call the function to fetch and display data
     fetchDataAndDisplay();
 });
